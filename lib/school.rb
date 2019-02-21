497af5994adc36d029832e900c251e496489a0ea
Zoe Bai <s.xue.bai@gmail.com>
2019-02-21 22:13:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,11 +10,11 @@ class School
   
   
   def add_student(name, grade)
-    if roster["#{grade}"]
-      roster[:"#{grade}""] << "#{name}"
+    if roster[:"#{grade}"]
+      roster[:"#{grade}"] << "#{name}"
     else
-      roster["#{grade}"] = []
-      roster["#{grade}"] << "#{name}"
+      roster[:"#{grade}"] = []
+      roster[:"#{grade}"] << "#{name}"
     end
   end
   
