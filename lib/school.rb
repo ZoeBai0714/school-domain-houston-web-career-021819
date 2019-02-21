4c4ee3e6f6bc824b9e3503312255246cd36a7783
Zoe Bai <s.xue.bai@gmail.com>
2019-02-21 22:02:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  roster = {}
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+ 
+  
+  def add_student(name, grade)
+    if roster["#{grade}"]
+      roster["#{grade}"] << "#{name}"
+    else
+      roster["#{grade}"] = []
+      roster["#{grade}"] << "#{name}"
+    end
+  end
+  
+  def grade(grade)
+    roster.each do |grades, students|
+      if grades == grade
+        return students
+    end
+  end
+  
+end
